9e1b35a9f437b96f666f96257b1a7dd9c004862b
NazgoooAtanasov <naskodragomirov@gmail.com>
2022-08-28 02:40:45 +0300

    adding dotenv to the project

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
 import users from './api/users';
 import prisma from './prisma';
 
+dotenv.config();
 const app = express();
 const prismaClient = prisma();
 
@@ -14,6 +16,6 @@ app.use(context);
 app.use(express.json());
 app.use('/api/v1', users);
 
-app.listen('8080', () => {
+app.listen(process.env.PORT || '8081', () => {
     console.log('running');
 });
